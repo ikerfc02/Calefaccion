1bfcf8979feeca303d4cd1108aac40e0b729c57d
ikerfc <ikerfc02@gmail.com>
2021-11-24 10:09:05 +0100

    01 Creado constructor y atributos de la clase termostato

---
+++ Termostato.java
@@ -0,0 +1,14 @@
+
+public class Termostato{
+    
+    private int temperaturaActual;
+    
+    private int temperaturaArranque;
+    
+    public Termostato(int temperaturaAhora, int temperaturaInicio){
+        temperaturaActual = temperaturaAhora;
+        temperaturaArranque = temperaturaInicio;
+    }
+    
+    
+}
