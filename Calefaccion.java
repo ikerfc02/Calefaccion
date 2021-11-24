6a0cb818b387fec7225910fce32681b54249dfd5
serac8 <92148837+serac8@users.noreply.github.com>
2021-11-24 10:24:47 +0100

    04 Añadidos atributos y constructor de la primera clase

---
+++ Calefaccion.java
@@ -0,0 +1,29 @@
+public class Calefaccion {
+
+    
+private boolean encendido;
+private int temperaturaMaxima;
+private Termostato termostato;
+
+
+public Calefaccion(boolean calefaccionEncendida, int tempMax, Termostato termostatoCalefaccion){
+    
+    calefaccionEncendida = encendido;
+    tempMax = temperaturaMaxima;
+    termostatoCalefaccion = termostato;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
