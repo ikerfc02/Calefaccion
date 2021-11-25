7d25b10f62a6d3b14abf33407bbcb968a41c7b0c
ikerfc <ikerfc02@gmail.com>
2021-11-25 13:14:08 +0100

    05 Incluidos metodos get y set en la clase calefaccion

--- Calefaccion.java
+++ Calefaccion.java
@@ -1,29 +1,41 @@
 public class Calefaccion {
 
     
-private boolean encendido;
-private int temperaturaMaxima;
-private Termostato termostato;
+    private boolean encendido;
+    private int temperaturaMaxima;
+    private Termostato termostato;
 
 
-public Calefaccion(boolean calefaccionEncendida, int tempMax, Termostato termostatoCalefaccion){
+    public Calefaccion(boolean calefaccionEncendida, int tempMax, Termostato termostatoCalefaccion){
     
-    calefaccionEncendida = encendido;
-    tempMax = temperaturaMaxima;
-    termostatoCalefaccion = termostato;
-}
+        encendido = calefaccionEncendida;
+        temperaturaMaxima = tempMax;
+        termostato = termostatoCalefaccion;
+    }
 
 
+    public void setEncendido(boolean calefaccionEncendida){
+        encendido = calefaccionEncendida;
+    }
 
+    public void setTemperaturaMaxima(int tempMax){
+        temperaturaMaxima = tempMax;
+    }
 
+    public void setTermostato(Termostato termostatoCalefaccion){
+        termostato = termostatoCalefaccion;
+    }
 
+    public boolean getEncendido(){
+        return encendido;
+    }
 
+    public int getTemperaturaMaxima(){
+        return temperaturaMaxima;
+    }
 
-
-
-
-
-
-
+    public Termostato getTermostato(){
+        return termostato;
+    }
 
 }
